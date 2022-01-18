e880aa8af717f7ae0cefcaa3492f29c61ff0f25a
gnopor <mayoublaise@gmail.com>
2022-01-18 07:13:24 +0100

    add home page and layout handler

--- src/App.js
+++ src/App.js
@@ -1,14 +1,77 @@
-/**
- * 
- * ! All auth related route must start with "/auth/some-path"
- */
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
+import DefaultLayout from "./layouts/DefaultLayout";
+// import AuthLayout from "./layouts/AuthLayout";
+
+import HomePage from "./pages/HomePage";
+
+
+
+import PrivateRoute from "./components/PrivateRoute";
+
+const routes = [
+  {
+    path: "/home",
+    page: HomePage,
+    layout: DefaultLayout,
+    protected: false,
+  },
+  {
+    path: "/auth/signup",
+    page: AuthenticationPage,
+    layout: DefaultLayout,
+    protected: false,
+  },
+  {
+    path: "/auth/activate-account",
+    page: AuthenticationPage,
+    layout: DefaultLayout,
+    protected: false,
+  },
+  {
+    path: "/auth/activate-account",
+    page: AuthenticationPage,
+    layout: DefaultLayout,
+    protected: false,
+  },
+];
 
 export default function App() {
   return (
-    <div >
-app
+    <div>
+      <BrowserRouter>
+        <HelmetProvider>
+          <Switch>
+            {routes.map((route) =>
+              route.protected ? (
+                <PrivateRoute
+                  key={route.path}
+                  exact
+                  path={route.path}
+                  render={(props) => (
+                    <route.layout>
+                      <route.page {...props} />
+                    </route.layout>
+                  )}
+                />
+              ) : (
+                <Route
+                  key={route.path}
+                  exact
+                  path={route.path}
+                  render={(props) => (
+                    <route.layout>
+                      <route.page {...props} />
+                    </route.layout>
+                  )}
+                />
+              )
+            )}
+            <Redirect to="/home" />
+          </Switch>
+        </HelmetProvider>
+      </BrowserRouter>
     </div>
   );
 }
-
-
---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+
+
+export default function HomePage() {
+  return (
+    <>
+      <Helmet>
+        <title>Accueil</title>
+      </Helmet>
+
+      <section>
+     home page
+      </section>
+    </>
+  );
+}
