9140b144a7f9eb83e32a44dcf58ba78bd34b72f4
Gnop <mayoublaise@gmail.com>
2022-04-20 16:58:58 +0100

    update localstorage helper

--- src/utilities/helpers/localStorage.helpers.js
+++ src/utilities/helpers/localStorage.helpers.js
@@ -1,17 +1,21 @@
 import Encryption from "./encryption.helpers";
 
 export default class LocalStorage {
-    static writeToLocalStorage(key, value) {
+    static setItem(key, value) {
         if (typeof key === "string" && typeof value === "string") {
             return localStorage.setItem(key, Encryption.encrypt(value));
         }
         throw new Error("key and value must be of type string.");
     }
 
-    static readFromLocalStorage(key) {
+    static getItem(key) {
         if (typeof key === "string") {
             return Encryption.decript(localStorage.getItem(key) || "") || "";
         }
         throw new Error("key and value must be of type string.");
+    }
+
+    static removeItem(key) {
+        return localStorage.removeItem(key);
     }
 }
