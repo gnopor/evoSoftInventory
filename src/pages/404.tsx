e7d1970edf584e5d4540a9c9b56583abdc1f4826
Gnop <mayoublaise@gmail.com>
2022-07-14 08:34:23 +0100

    fix 404 page browser check issue

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 export default function Error404Page() {
     const router = useRouter();
 
-    if (typeof window !== undefined) {
+    if (typeof window !== "undefined") {
         router.push("/home/");
     }
 
