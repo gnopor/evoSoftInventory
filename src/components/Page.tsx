328d5f79d9e6a057055ca8cc1f79a789196011ff
Gnop <mayoublaise@gmail.com>
2022-09-12 21:03:25 +0100

    add the next head component in the page component

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 
 import DefaultLayout from "../layouts/DefaultLayout";
 import PathHelpers from "../utilities/helpers/path.helpers";
 import { useAuth } from "../stores/authenticationStore/authContext";
 import { APP_NAME } from "../constants";
+import Head from "next/head";
 
 interface IProps {
     children: React.ReactNode;
@@ -24,20 +25,23 @@ export default function Page({ children, layout, title, private: isPrivate }: IP
 
     const CurrentLayout = LAYOUTS_MAP[layout || "DEFAULT_LAYOUT"];
 
-    useEffect(() => {
-        title?.trim() && setPageTitle(title);
-    }, [title]);
-
-    const setPageTitle = (newTitle: string) => {
-        document.title = `${newTitle.trim()} | ${APP_NAME}`;
-    };
-
-    if (typeof window === "undefined") return <></>;
-
+    if (isPrivate && typeof document === "undefined") return <></>;
     if (isPrivate && !isUserAuthenticated()) {
         router.push(PathHelpers.loginPagePath());
         return <></>;
     }
 
-    return <CurrentLayout>{children}</CurrentLayout>;
+    return (
+        <>
+            {title?.trim() && (
+                <Head>
+                    <title>
+                        {title.trim()} | {APP_NAME}
+                    </title>
+                </Head>
+            )}
+
+            <CurrentLayout>{children}</CurrentLayout>
+        </>
+    );
 }
