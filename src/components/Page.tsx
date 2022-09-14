33a41f0b78fdf399c4531d1d5c3d185e0be3a3f9
Gnop <mayoublaise@gmail.com>
2022-09-14 19:36:16 +0100

    add anonym only page validator

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 import DefaultLayout from "../layouts/DefaultLayout";
@@ -12,22 +12,35 @@ interface IProps {
     layout?: keyof typeof LAYOUTS_MAP;
     title?: string;
     private?: boolean;
+    anonymOnly?: boolean;
 }
 
 const LAYOUTS_MAP = {
     DEFAULT_LAYOUT: DefaultLayout
 };
 
-export default function Page({ children, layout, title, private: isPrivate }: IProps) {
+export default function Page({ children, layout, title, private: isPrivate, anonymOnly }: IProps) {
     const { isUserAuthenticated } = useAuth();
 
     const router = useRouter();
 
+    const [isBrowser, setIsBrowser] = useState(false);
+
     const CurrentLayout = LAYOUTS_MAP[layout || "DEFAULT_LAYOUT"];
 
-    if (isPrivate && typeof document === "undefined") return <></>;
+    useEffect(() => {
+        setIsBrowser(true);
+    }, []);
+
+    if ((isPrivate || anonymOnly) && !isBrowser) return <></>;
+
     if (isPrivate && !isUserAuthenticated()) {
         router.push(PathHelpers.loginPagePath());
+        return <></>;
+    }
+
+    if (anonymOnly && isUserAuthenticated()) {
+        router.push(PathHelpers.homePagePath());
         return <></>;
     }
 
