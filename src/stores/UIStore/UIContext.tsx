5ab6a459fca7e1853ec965e355bcf225c6b70463
Gnop <mayoublaise@gmail.com>
2022-10-10 10:30:21 +0100

    add the new error free ui context

--- src/stores/UIStore/UIContext.tsx
+++ src/stores/UIStore/UIContext.tsx
@@ -1,134 +1,134 @@
-/* eslint-disable max-lines-per-function */
-import React, { useEffect, useState } from "react";
+// /* eslint-disable max-lines-per-function */
+// import React, { useEffect, useState } from "react";
 
-import { DEFAULT_LANGUAGE_CODE, localStorageFields, PHONE_SCREEN_SIZE } from "../../constants";
-import CircularProgress from "../../components/CircularProgress";
-import uiService from "../../services/ui.service";
-import Helpers from "../../utilities/helpers/helpers";
-import SecureLocalStorage from "../../utilities/helpers/secureLocalStorage.helpers";
-import PathHelpers from "../../utilities/helpers/path.helpers";
+// import { DEFAULT_LANGUAGE_CODE, localStorageFields, PHONE_SCREEN_SIZE } from "../../constants";
+// import CircularProgress from "../../components/CircularProgress";
+// import uiService from "../../services/ui.service";
+// import Helpers from "../../utilities/helpers/helpers";
+// import SecureLocalStorage from "../../utilities/helpers/secureLocalStorage.helpers";
+// import PathHelpers from "../../utilities/helpers/path.helpers";
 
-// copied from the linter of value
-interface IUIContext {
-    languages: I.ILanguage[] | undefined;
-    currentLanguage: I.ILanguage | undefined;
-    label: I.ILabel | undefined;
-    isPhoneView: boolean;
-    updateCurrentLanguage: (language: any) => void;
-    languageMap: { [key: string]: I.ILanguage } | undefined;
-    currentPath: string | undefined;
-}
+// // copied from the linter of value
+// interface IUIContext {
+//     languages: I.ILanguage[] | undefined;
+//     currentLanguage: I.ILanguage | undefined;
+//     label: I.ILabel | undefined;
+//     isPhoneView: boolean;
+//     updateCurrentLanguage: (languageCode2: string) => void;
+//     languageMap: { [key: string]: I.ILanguage } | undefined;
+//     currentPath: string | undefined;
+// }
 
-const UIContext = React.createContext({} as IUIContext);
+// const UIContext = React.createContext({} as IUIContext);
 
-function useUI() {
-    const context = React.useContext(UIContext);
+// function useUI() {
+//     const context = React.useContext(UIContext);
 
-    if (context === undefined) {
-        throw new Error("UIContext must be used within an UIProvider.");
-    }
+//     if (context === undefined) {
+//         throw new Error("UIContext must be used within an UIProvider.");
+//     }
 
-    return context;
-}
+//     return context;
+// }
 
-function UIProvider({ children }: { children: React.ReactNode }) {
-    const [languages, setLanguages] = useState<I.ILanguage[]>();
-    const [languageMap, setLanguageMap] = useState<{ [key: string]: I.ILanguage }>();
-    const [currentLanguage, setCurrentLanguage] = useState<I.ILanguage>();
+// function UIProvider({ children }: { children: React.ReactNode }) {
+//     const [languages, setLanguages] = useState<I.ILanguage[]>();
+//     const [languageMap, setLanguageMap] = useState<{ [key: string]: I.ILanguage }>();
+//     const [currentLanguage, setCurrentLanguage] = useState<I.ILanguage>();
 
-    const [label, setLabel] = useState<I.ILabel>();
+//     const [label, setLabel] = useState<I.ILabel>();
 
-    const [isPhoneView, setIsPhoneView] = useState(false);
-    const [currentPath, setCurrentPath] = useState<string>();
+//     const [isPhoneView, setIsPhoneView] = useState(false);
+//     const [currentPath, setCurrentPath] = useState<string>();
 
-    useEffect(() => {
-        Object.keys(label || {}).length === 0 && initState();
-    }, [label]);
+//     useEffect(() => {
+//         Object.keys(label || {}).length === 0 && initState();
+//     }, [label]);
 
-    const initState = async () => {
-        handleSetPhoneViewState();
-        handleSetCurrentPath();
+//     const initState = async () => {
+//         handleSetPhoneViewState();
+//         handleSetCurrentPath();
 
-        const language = await handleSetLanguageState();
-        language && handleSetLabel(language);
-    };
+//         const language = await handleSetLanguageState();
+//         language && handleSetLabel(language);
+//     };
 
-    const handleSetPhoneViewState = () => {
-        setIsPhoneView(Helpers.getScreenWidth() < PHONE_SCREEN_SIZE);
-        Helpers.onScreenSizeChange((size: number) => setIsPhoneView(size < PHONE_SCREEN_SIZE));
-    };
+//     const handleSetPhoneViewState = () => {
+//         setIsPhoneView(Helpers.getScreenWidth() < PHONE_SCREEN_SIZE);
+//         Helpers.onScreenSizeChange((size: number) => setIsPhoneView(size < PHONE_SCREEN_SIZE));
+//     };
 
-    const handleSetCurrentPath = () => {
-        PathHelpers.onPathChange((newPath) => setCurrentPath(newPath));
-    };
+//     const handleSetCurrentPath = () => {
+//         PathHelpers.onPathChange((newPath) => setCurrentPath(newPath));
+//     };
 
-    const handleSetLabel = async (language: I.ILanguage) => {
-        const label = await uiService.getLabel(language);
-        setLabel(label);
-    };
+//     const handleSetLabel = async (language: I.ILanguage) => {
+//         const label = await uiService.getLabel(language);
+//         setLabel(label);
+//     };
 
-    const handleSetLanguageState = async () => {
-        const languagesList = await uiService.getLanguages();
-        const languageCode = window.location.pathname.split("/")[1] || getDefaultLanguage();
+//     const handleSetLanguageState = async () => {
+//         const languagesList = await uiService.getLanguages();
+//         const languageCode = getDefaultLanguage();
 
-        const language = languagesList.find((l) => l.code2 === languageCode);
-        if (!language) {
-            window.open(PathHelpers.homePagePath(languageCode), "_self");
-            return;
-        }
+//         const language = languagesList.find((l) => l.code2 === languageCode);
+//         if (!language) {
+//             window.open(PathHelpers.homePagePath(languageCode), "_self");
+//             return;
+//         }
 
-        const newLanguageMap = Helpers.getMap(languagesList, "code2");
+//         const newLanguageMap = Helpers.getMap(languagesList, "code2");
 
-        setLanguageMap(newLanguageMap);
-        setLanguages(languagesList);
-        setCurrentLanguage(language);
-        saveDefaultLanguage(languageCode);
+//         setLanguageMap(newLanguageMap);
+//         setLanguages(languagesList);
+//         setCurrentLanguage(language);
+//         saveDefaultLanguage(languageCode);
 
-        return language;
-    };
+//         return language;
+//     };
 
-    const updateCurrentLanguage = (languageCode2: string) => {
-        if (languageCode2 === getDefaultLanguage()) return;
+//     const updateCurrentLanguage = (languageCode2: string) => {
+//         if (languageCode2 === getDefaultLanguage()) return;
 
-        const oldPath = window.location.pathname;
+//         const oldPath = window.location.pathname;
 
-        const pathParts = oldPath.split("/");
-        pathParts[1] = languageCode2;
+//         const pathParts = oldPath.split("/");
+//         pathParts[1] = languageCode2;
 
-        saveDefaultLanguage(languageCode2);
-        window.open(pathParts.join("/"), "_self");
-    };
+//         saveDefaultLanguage(languageCode2);
+//         window.open(pathParts.join("/"), "_self");
+//     };
 
-    const saveDefaultLanguage = (languageCode2: string) => {
-        return SecureLocalStorage.setItem(localStorageFields.DEFAULT_USER_LANGUAGE, languageCode2);
-    };
+//     const saveDefaultLanguage = (languageCode2: string) => {
+//         return SecureLocalStorage.setItem(localStorageFields.DEFAULT_USER_LANGUAGE, languageCode2);
+//     };
 
-    const getDefaultLanguage = () => {
-        const languageCode = SecureLocalStorage.getItem(localStorageFields.DEFAULT_USER_LANGUAGE);
-        return languageCode || DEFAULT_LANGUAGE_CODE;
-    };
+//     const getDefaultLanguage = () => {
+//         const languageCode = SecureLocalStorage.getItem(localStorageFields.DEFAULT_USER_LANGUAGE);
+//         return languageCode || DEFAULT_LANGUAGE_CODE;
+//     };
 
-    const value = {
-        languages,
-        currentLanguage,
-        label,
-        isPhoneView,
-        updateCurrentLanguage,
-        languageMap,
-        currentPath
-    };
+//     const value = {
+//         languages,
+//         currentLanguage,
+//         label,
+//         isPhoneView,
+//         updateCurrentLanguage,
+//         languageMap,
+//         currentPath
+//     };
 
-    return (
-        <UIContext.Provider value={value}>
-            {Object.keys(label || {}).length > 0 ? (
-                children
-            ) : (
-                <div className="vh-100 w-100 d-flex justify-content-center align-items-center">
-                    <CircularProgress />
-                </div>
-            )}
-        </UIContext.Provider>
-    );
-}
+//     return (
+//         <UIContext.Provider value={value}>
+//             {Object.keys(label || {}).length > 0 ? (
+//                 children
+//             ) : (
+//                 <div className="vh-100 w-100 d-flex justify-content-center align-items-center">
+//                     <CircularProgress />
+//                 </div>
+//             )}
+//         </UIContext.Provider>
+//     );
+// }
 
-export { UIProvider, useUI };
+// export { UIProvider, useUI };
--- src/utilities/helpers/path.helpers.ts
+++ src/utilities/helpers/path.helpers.ts
@@ -1,4 +1,36 @@
 export default class PathHelpers {
+        static onPathChange(callback: (newPath?: string) => void) {
+        const element = window.document;
+        let previousPath = window.location.pathname;
+
+        callback?.(previousPath);
+
+        const observer = new MutationObserver((mutationRecords) => {
+            mutationRecords.every(() => {
+                if (previousPath !== window.location.pathname) {
+                    previousPath = window.location.pathname;
+                    callback?.(previousPath);
+                    return false;
+                }
+                return true;
+            });
+        });
+
+        const options = { subtree: true, attributes: true };
+        observer.observe(element, options);
+    }
+
+    static getPath(str: string, languageCode: ILanguageCode = "en") {
+        const pathName = window.location.pathname;
+
+        const language = pathName[3] === "/" ? pathName.slice(0, 3) : `/${languageCode}`;
+        const path = String(str).trim();
+
+        return `${language}${path}`;
+    }
+
+    // -----------------------------
+    
     static error404Path() {
         return "/404/";
     }
