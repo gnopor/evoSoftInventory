921503b565b999af16f83a99c073a25bb8cf19fc
Gnop <mayoublaise@gmail.com>
2022-11-15 21:05:56 +0100

    update the default layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,13 +1,38 @@
 import React from "react";
-// import Footer from "../components/Footer";
-// import Header from "../components/Header";
+import css from "styled-jsx/css";
+
+// import Header from "../components/headers/Header";
+// import Footer from "../components/footers/Footer";
 
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            {/* <Header /> */}
-            {children}
-            {/* <Footer /> */}
+            <div id="default_layout">
+                {/* <Header /> */}
+                {children}
+                {/* <Footer /> */}
+            </div>
+
+            <style jsx global>
+                {style}
+            </style>
         </>
     );
 }
+
+const style = css.global`
+    #default_layout {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        width: 100vw;
+        max-width: 100vw;
+        overflow-x: hidden;
+        overflow-y: auto;
+    }
+
+    #default_layout main {
+        flex: 1 0 auto;
+        width: 100%;
+    }
+`;
