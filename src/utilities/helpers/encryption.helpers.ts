be982887d5d1397926c51014ad9a24528b0e10a5
Blaise TAYOU <mayoublaise@gmail.com>
2024-04-18 10:11:23 +0100

    encryptionHelpers: refactor

--- src/utilities/helpers/encryption.helpers.ts
+++ src/utilities/helpers/encryption.helpers.ts
@@ -6,12 +6,14 @@ const SECRET_ENCRIPTION_KEY = fingerprintHelpers.getFingerprint();
 
 export default class EncryptionHelpers {
     static encrypt(plainText: string, secretKey = SECRET_ENCRIPTION_KEY) {
-        return AES.encrypt(plainText, secretKey).toString();
+        return AES.encrypt(JSON.stringify(plainText), secretKey).toString();
     }
 
     static decript(cipherText: string, secretKey = SECRET_ENCRIPTION_KEY) {
         try {
-            return AES.decrypt(cipherText, secretKey).toString(enc.Utf8);
+            if (!cipherText.trim()) return "";
+
+            return JSON.parse(AES.decrypt(cipherText, secretKey).toString(enc.Utf8));
         } catch (error) {
             console.error(error);
         }
