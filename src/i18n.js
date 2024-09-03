051da6794c118020faebb9d134de4c819550a648
Blaise TAYOU <mayoublaise@gmail.com>
2024-09-03 13:56:10 +0100

    Move labels to the public dir

--- src/i18n.js
+++ src/i18n.js
@@ -2,18 +2,12 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-import enResources from "./assets/json/i18n/en.resources.json";
-import frResources from "./assets/json/i18n/fr.resources.json";
+import resources from "../public/json/i18n.resources.json";
 
-i18n.use(initReactI18next)
-    .use(LanguageDetector)
-    .init({
-        lng: "en",
-        fallbackLng: "en",
-        resources: {
-            en: enResources,
-            fr: frResources
-        }
-    });
+i18n.use(initReactI18next).use(LanguageDetector).init({
+    lng: "en",
+    fallbackLng: "en",
+    resources
+});
 
 export default i18n;
